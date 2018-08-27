4bce0a49256b4a35c6752060d6bfbb4afaa71138
jorge-gt3 <jorgeg@gate3.co>
2018-08-27 09:59:41 -0500

    updated comments

--- ad_post.ps1
+++ ad_post.ps1
@@ -3,32 +3,35 @@
 #           The script queries the users information from AD and sends them to Greetly via HTTP Post
 #           
 ###########################################################
-# Set credentials of user querying AD
-# We recommend not to enter plan text passwords of priviledge accounts
-# A restricted service account has enough permissions to query the AD properties Greetly needs
+# In order to use this script, you need to update the following variables
+# username: We recommend not to enter plan text passwords of priviledge accounts. A restricted service account has enough permissions to query the AD properties Greetly needs
+# password: The password of the account above.
+# APIKey: Your API-KEY provided by Greetly. You can find this on the Account Settings page.
+# SearchBase: The OU in AD where users will be retrived from
+# ADServer: The name of AD server queries will be performed against
+# 
 $username = "domain\username"
 $password = "password"
-#
-# Enter your Greetly API KEY
 $APIKey="your_greetly_api_key_goes_here"
-#
-# Sets the OU to do the base search for all user accounts, change as required.
 $SearchBase = "CN=Users,DC=your_domain,DC=local"
-# Define variable for a server with AD web services installed
 $ADServer = 'YOUR_SERVER_NAME'
+#
+###########################################################
 #
 $securePassword = ConvertTo-SecureString $password -AsPlainText -Force
 $userCredential = New-Object System.Management.Automation.PSCredential ($username, $securePassword)
-#
+
 # Import the ActiveDirectory Module
 Import-Module ActiveDirectory
+
 # Filter users if you want to send only active AD users
 $filterString = 'enabled -eq $true'
-#
+
 $ADUsers = Get-ADUser -server $ADServer -Credential $userCredential -searchbase $SearchBase -Filter $filterString -Properties *
 $httpBody =  $ADUsers | Select-Object @{Label = "email";Expression = {$_.Mail}},
 @{Label = "first_name";Expression = {$_.GivenName}},
 @{Label = "last_name";Expression = {$_.Surname}},
+
 # Since greetly can be adapted to different industries, we suggest using the following field to what better suits your needs
 @{Label = "company";Expression = {$_.Title}},
 #@{Label = "company";Expression = {$_.Company}},
@@ -38,9 +41,10 @@ $httpBody =  $ADUsers | Select-Object @{Label = "email";Expression = {$_.Mail}},
 # These fields are not in AD, but could be stored as custom attributes
 #@{Label = "voice_extension";Expression = {$_.customAttribute1}},
 #@{Label = "slack_handle";Expression = {$_.customAttribute2}},
-#
+
 # Set headers needed by API
 $headers = @{"API-KEY"=$APIKey} @{"API-KEY"=$APIKey; "Content-Type" = "text/csv"}
+
 # Set to Greetly via POST
 Invoke-WebRequest -Uri https://app.greetly.com/api/directory_services -Method POST -Body $httpBody -Headers $headers
 
